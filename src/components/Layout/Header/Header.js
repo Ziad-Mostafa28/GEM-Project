d4c77f4b4d2747a38311bb1b6822f1f389b32187
Ziad-Mostafa28 <ziad46363@gmail.com>
2023-10-18 18:19:28 +0300

    edit class in header

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -136,8 +136,8 @@ const Header = () => {
                         </span>
                     </a>
                     <button aria-controls="basic-navbar-nav" type="button"
-                        aria-label="Toggle navigation" class="navbar-toggler collapsed">
-                        <span class="navbar-toggler-icon">
+                        aria-label="Toggle navigation" className="navbar-toggler collapsed">
+                        <span className="navbar-toggler-icon">
                         </span>
                     </button>
                     <div className={`navbar_box_nav navbar-collapse collapse ${classes.navsBar}`} >
@@ -151,7 +151,7 @@ const Header = () => {
                                             <div className={`${classes.input_Search}`}>
                                                 <input className={`${classes.Search_input}`} type="search" id="searchQuery" />
                                                 <button className={` ${classes.Search_button}`}>
-                                                    <svg className={`w-6 ${classes.Svgss}`} xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" fill="none" stroke-width="1.5">
+                                                    <svg className={`w-6 ${classes.Svgss}`} xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" fill="none" strokeWidth="1.5">
                                                         <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
                                                     </svg>
                                                 </button>
@@ -160,7 +160,7 @@ const Header = () => {
                                     </div> */}
                                 </li>
                                 <li className={`${classes.All_Li}`}>
-                                    <svg className={`w-6 ${classes.Svgssss}`} xmlns="http://www.w3.org/2000/svg" fill="none" height="1em" stroke-width="1.5" viewBox="0 0 512 512">
+                                    <svg className={`w-6 ${classes.Svgssss}`} xmlns="http://www.w3.org/2000/svg" fill="none" height="1em" strokeWidth="1.5" viewBox="0 0 512 512">
                                         <path d="M352 256c0 22.2-1.2 43.6-3.3 64H163.3c-2.2-20.4-3.3-41.8-3.3-64s1.2-43.6 3.3-64H348.7c2.2 20.4 3.3 41.8 3.3 64zm28.8-64H503.9c5.3 20.5 8.1 41.9 8.1 64s-2.8 43.5-8.1 64H380.8c2.1-20.6 3.2-42 3.2-64s-1.1-43.4-3.2-64zm112.6-32H376.7c-10-63.9-29.8-117.4-55.3-151.6c78.3 20.7 142 77.5 171.9 151.6zm-149.1 0H167.7c6.1-36.4 15.5-68.6 27-94.7c10.5-23.6 22.2-40.7 33.5-51.5C239.4 3.2 248.7 0 256 0s16.6 3.2 27.8 13.8c11.3 10.8 23 27.9 33.5 51.5c11.6 26 20.9 58.2 27 94.7zm-209 0H18.6C48.6 85.9 112.2 29.1 190.6 8.4C165.1 42.6 145.3 96.1 135.3 160zM8.1 192H131.2c-2.1 20.6-3.2 42-3.2 64s1.1 43.4 3.2 64H8.1C2.8 299.5 0 278.1 0 256s2.8-43.5 8.1-64zM194.7 446.6c-11.6-26-20.9-58.2-27-94.6H344.3c-6.1 36.4-15.5 68.6-27 94.6c-10.5 23.6-22.2 40.7-33.5 51.5C272.6 508.8 263.3 512 256 512s-16.6-3.2-27.8-13.8c-11.3-10.8-23-27.9-33.5-51.5zM135.3 352c10 63.9 29.8 117.4 55.3 151.6C112.2 482.9 48.6 426.1 18.6 352H135.3zm358.1 0c-30 74.1-93.6 130.9-171.9 151.6c25.5-34.2 45.2-87.7 55.3-151.6H493.4z" />
                                     </svg>
                                     <div className={` end-0 ${classes.dropdown_nav}`}>
@@ -169,7 +169,7 @@ const Header = () => {
                                     </div>
                                 </li>
                                 <li className={`${classes.All_Li}`}>
-                                    <svg className={`w-6 ${classes.Svgssss}`} xmlns="http://www.w3.org/2000/svg" fill="none" stroke-width="1.5" height="1em" viewBox="0 0 448 512">
+                                    <svg className={`w-6 ${classes.Svgssss}`} xmlns="http://www.w3.org/2000/svg" fill="none" strokeWidth="1.5" height="1em" viewBox="0 0 448 512">
                                         <path d="M304 128a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM49.3 464H398.7c-8.9-63.3-63.3-112-129-112H178.3c-65.7 0-120.1 48.7-129 112zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3z" />
                                     </svg>
                                     <div className={` end-0 ${classes.dropdown_nav}`}>
