f58c1f54ceb954b9876b0ad279b750048e36d06b
Ziad-Mostafa28 <ziad46363@gmail.com>
2023-11-01 17:05:03 +0200

    libarary

--- src/index.js
+++ src/index.js
@@ -5,12 +5,38 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import { BrowserRouter } from 'react-router-dom';
-
-
+import i18n from "i18next";
+import global_en from './translations/en/global.json';
+import global_ar from './translations/ar/global.json'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const lang = localStorage.getItem('language')
+
+if (lang === 'en') {
+  document.querySelector('html').dir = 'ltr'
+} else {
+  document.querySelector('html').dir = 'rtl'
+
+}
+
+i18n
+  .init({
+    resources: {
+      en: {
+        global: global_en
+      },
+      ar: {
+        global: global_ar
+      },
+    },
+    lng: "en", // if you're using a language detector, do not define the lng option
+    fallbackLng: "en",
+ 
+  });
+
+
 root.render(
   < BrowserRouter>
-    <App />
+      <App />
   </ BrowserRouter>
 );
