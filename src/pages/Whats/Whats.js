083e1c9eb38d3d568f957d22a43d74da65b759a4
Ziad-Mostafa28 <ziad46363@gmail.com>
2023-11-07 15:13:40 +0200

    add page

--- src/pages/Whats/Whats.js
+++ src/pages/Whats/Whats.js
@@ -2,11 +2,18 @@ import React, { useEffect, useState } from 'react'
 import endPoints, { httpData } from '../../store/Alldata';
 import TopImage from '../../components/wrapper/TopImage';
 import style from './Whats.module.css'
+import Loader from '../../components/wrapper/Loader';
 
 const Whats = () => {
 
     const [whatsdata, setwhatsdata] = useState([]);
+    const [events, setEvents] = useState();
+    const [currentdata, setcurrentdata] = useState([]);
+    const [loader, setLoader] = useState(true);
 
+    const changeEvents = (tab) => {
+        setEvents(tab)
+    }
     useEffect(() => {
         async function getwhatsData() {
             const response = await httpData.get(endPoints.whats);
@@ -15,10 +22,17 @@ const Whats = () => {
         getwhatsData();
     }, []);
 
-    const [flag, setFlag] = useState(false);
-    const handleClick = () => {
-        setFlag(!flag)
-    }
+
+    useEffect(() => {
+        async function getcurrentData() {
+            const response = await httpData.get(`${endPoints.Current}${events ? `&category_id=${events}` : ''}`);
+            setcurrentdata(response.data.data);
+            setLoader(false)
+            console.log(response);
+        }
+        getcurrentData();
+    }, [events]);
+
 
     return (
 
@@ -43,22 +57,10 @@ const Whats = () => {
                             <div className='container'>
                                 <div className='row'>
                                     <div className='align-self-center col-6'>
-                                        {/* <ul className={`list-unstyled mb-0 ${style.search_searchBar_links}`}>
-                                            <li className={`${style.search_searchBar_links_linkk}`}>
-                                                <a className={`${style.aaa}`} href='#'>
-                                                    Current
-                                                </a>
-                                            </li>
-                                            <li className={`${style.search_searchBar_links_linkk}`}>
-                                                <a className={`${style.aaa}`} href='#'>
-                                                    Upcoming
-                                                </a>
-                                            </li>
-                                        </ul> */}
                                         <nav>
                                             <div className={`nav nav-tabs list-unstyled mb-0 ${style.search_searchBar_links}`} id="nav-tab" role="tablist">
-                                                <button className={`nav-link active ${style.search_searchBar_links_linkk}`} id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Home</button>
-                                                <button className={`nav-link ${style.search_searchBar_links_linkk}`} id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</button>
+                                                <button className={`nav-link active ${style.search_searchBar_links_linkk}`} id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Current</button>
+                                                <button className={`nav-link ${style.search_searchBar_links_linkk}`} id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Upcoming</button>
                                             </div>
                                         </nav>
                                     </div>
@@ -72,11 +74,84 @@ const Whats = () => {
 
             <section className={`${style.seconed_section}`}>
                 <div className='container'>
-                    <div class="tab-content" id="nav-tabContent">
-                        <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab" tabindex="0">
-                            ...
-                            
+                    <div className="tab-content" id="nav-tabContent">
+                        <div className="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab" tabindex="0">
+
+                            <nav>
+                                <div className="nav nav-tabs" id="nav-tab" role="tablist">
+                                    <button className="nav-link active" id="nav-All-tab" data-bs-toggle="tab" data-bs-target="#nav-All" type="button" role="tab" aria-controls="nav-All" aria-selected="true">All</button>
+                                    <button className="nav-link" id="nav-Events-tab" data-bs-toggle="tab" data-bs-target="#nav-Events" type="button" role="tab" aria-controls="nav-Events" aria-selected="false" onClick={() => changeEvents(1)}>Events</button>
+                                    <button className="nav-link" id="nav-Conferences-tab" data-bs-toggle="tab" data-bs-target="#nav-Conferences" type="button" role="tab" aria-controls="nav-Conferences" aria-selected="false" onClick={() => changeEvents(2)}>Conferences</button>
+                                    <button className="nav-link" id="nav-Cultural-Programs-tab" data-bs-toggle="tab" data-bs-target="#nav-Cultural-Programs" type="button" role="tab" aria-controls="nav-Cultural-Programs" aria-selected="false" Cultural-Programs onClick={() => changeEvents(3)}>Cultural-Programs</button>
+                                </div>
+                            </nav>
+                            <div className="tab-content" id="nav-tabContent">
+                                <div className="tab-pane fade show active" id="nav-All" role="tabpanel" aria-labelledby="nav-All-tab" tabindex="0">
+                                    {loader ? <Loader /> : <div className='row'>
+                                            <div className='mb-4 col-lg-4 col-md-6'>
+                                                {currentdata?.events && currentdata?.events.map((data) =>
+                                                    <div key={data.id} className={`${style.whatson_card}`}>
+                                                        <div className='img-block zoomIn h-100 undefined'>
+                                                            <span className={`d-flex h-100 ${style.First_Bottom_Span}`}>
+                                                                <img key={data.id} src={data.thumbnail} className={`img-block zoomIn h-100 w-100 undefined`} />
+                                                            </span>
+                                                        </div>
+                                                    </div>
+                                                )}
+                                            </div>
+                                        </div>
+                                    }
+                                </div>
+                                <div class="tab-pane fade" id="nav-Events" role="tabpanel" aria-labelledby="nav-Events-tab" tabindex="0">
+                                    {loader ? <Loader /> : <div className='row'>
+                                            <div className='mb-4 col-lg-4 col-md-6'>
+                                                {currentdata?.events && currentdata?.events.map((data) =>
+                                                    <div key={data.id} className={`${style.whatson_card}`}>
+                                                        <div className='img-block zoomIn h-100 undefined'>
+                                                            <span className={`d-flex h-100 ${style.First_Bottom_Span}`}>
+                                                                <img key={data.id} src={data.thumbnail} className={`img-block zoomIn h-100 w-100 undefined`} />
+                                                            </span>
+                                                        </div>
+                                                    </div>
+                                                )}
+                                            </div>
+                                        </div>
+                                    }
+                                </div>
+                                <div class="tab-pane fade" id="nav-Conferences" role="tabpanel" aria-labelledby="nav-Conferences-tab" tabindex="0">
+                                    {loader ? <Loader /> : <div className='row'>
+                                            <div className='mb-4 col-lg-4 col-md-6'>
+                                                {currentdata?.events && currentdata?.events.map((data) =>
+                                                    <div key={data.id} className={`${style.whatson_card}`}>
+                                                        <div className='img-block zoomIn h-100 undefined'>
+                                                            <span className={`d-flex h-100 ${style.First_Bottom_Span}`}>
+                                                                <img key={data.id} src={data.thumbnail} className={`img-block zoomIn h-100 w-100 undefined`} />
+                                                            </span>
+                                                        </div>
+                                                    </div>
+                                                )}
+                                            </div>
+                                        </div>
+                                    }
+                                </div>
+                                <div class="tab-pane fade" id="nav-Cultural-Programs" role="tabpanel" aria-labelledby="nav-Cultural-Programs-tab" tabindex="0">
+                                    {loader ? <Loader /> : <div className='row'>
+                                            <div className='mb-4 col-lg-4 col-md-6'>
+                                                {currentdata?.events && currentdata?.events.map((data) =>
+                                                    <div key={data.id} className={`${style.whatson_card}`}>
+                                                        <div className='img-block zoomIn h-100 undefined'>
+                                                            <span className={`d-flex h-100 ${style.First_Bottom_Span}`}>
+                                                                <img key={data.id} src={data.thumbnail} className={`img-block zoomIn h-100 w-100 undefined`} />
+                                                            </span>
+                                                        </div>
+                                                    </div>
+                                                )}
+                                            </div>
+                                        </div>
+                                    }
+                                </div>
                             </div>
+                        </div>
                         <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab" tabindex="0">...</div>
                     </div>
                 </div>
