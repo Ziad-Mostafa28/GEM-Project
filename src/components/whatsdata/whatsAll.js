1ffccfa82e01dc801fd91316bb7bea29ff773781
Ziad-Mostafa28 <ziad46363@gmail.com>
2023-11-07 16:00:45 +0200

    new component

--- src/components/whatsdata/whatsAll.js
+++ src/components/whatsdata/whatsAll.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import style from './WhatsAll.module.css'
 
-function whatsAll() {
+
+const WhatsAll = ({ data }) => {
+
     return (
-        <div>
+        <>
+            <div className='row'>
+                <div className='mb-4 col-lg-4 col-md-6'>
+                    {data && data.map((data) =>
+                        <div key={data.id} className={`${style.whatson_card}`}>
+                            <div className='img-block zoomIn h-100 undefined'>
+                                <span className={`d-flex h-100 ${style.First_Bottom_Span}`}>
+                                    <img key={data.id} src={data.thumbnail} className={`img-block zoomIn h-100 w-100 undefined`} />
+                                </span>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </div>
 
-        </div>
+        </>
     )
 }
 
-export default whatsAll
+export default WhatsAll
