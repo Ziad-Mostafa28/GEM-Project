3c646d7c5c0af1edb274ec4a2e35c7eb07796625
Ziad-Mostafa28 <ziad46363@gmail.com>
2023-11-08 13:55:24 +0200

    add page

--- src/pages/MuseumCollection/MuseCollection.js
+++ src/pages/MuseumCollection/MuseCollection.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import endPoints, { httpData } from '../../store/Alldata';
 import style from './MuseCollection.module.css'
+import TopImage from '../../components/wrapper/TopImage';
 
 const MuseCollection = () => {
 
@@ -16,8 +17,36 @@ const MuseCollection = () => {
 
     return (
 
-        <section className={`${style.welcome}`}>
-            
+        <section >
+            <TopImage>
+                <span>
+                    <img src={musecollection.banner_image} />
+                </span>
+                <div>
+                    <div className='container'>
+                        <h3>
+                            <a href='#'>
+                                <span className={`${style.spsp}`}>Gem -  What's on |</span> <span className={`${style.spsp}`}>{musecollection.banner_title}</span> </a>
+                        </h3>
+                        <p>
+                            {musecollection.banner_description}
+
+                        </p>
+                    </div>
+                    <form className={`${style.Search_searchBar}`}>
+                        <div className='container'>
+                            <div className={`${style.search_search_group}`}>
+                                <div className={`${style.search_search_group_div}`}>
+                                    <input className={`${style.search_search_group_controlForm}`} type='search' placeholder='Search' id='collectionSearch' />
+                                    <button className={`${style.search_search_group_link}`} aria-label='collectionSearch' id='searchSubmit'>
+                                        Search
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </TopImage>
         </section>
     )
 }
