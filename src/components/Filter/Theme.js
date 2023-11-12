82bacaa6db4e8bc073ad023236952a098ea39a4f
Ziad-Mostafa28 <ziad46363@gmail.com>
2023-11-12 11:54:16 +0200

    new option

---
+++ src/components/Filter/Theme.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react'
+import endPoints, { httpData } from '../../store/Alldata';
+import style from './Theme.module.css'
+
+function Theme() {
+
+    const [themedata, setThemedata] = useState([]);
+
+    useEffect(() => {
+        async function getthemedata() {
+            const response = await httpData.get(endPoints.Theme);
+            setThemedata(response.data.data);
+        }
+        getthemedata();
+    }, []);
+    return (
+
+        <select className={style.Div_select}>
+            <option selected >Select</option>
+            {themedata && themedata.map((data) =>
+
+                <option value={data?.id}>
+                    {data?.title}
+                </option>
+            )}
+        </select>
+    )
+}
+
+export default Theme
