7bb47b853436360b53ede1d286db000092401aaf
Ziad-Mostafa28 <ziad46363@gmail.com>
2023-11-19 13:04:41 +0200

    filter collection

--- src/components/Filter/Provenance.js
+++ src/components/Filter/Provenance.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import endPoints, { httpData } from '../../store/Alldata';
 import style from './Provenance.module.css'
 
-function Provenance() {
+function Provenance({ provenanceValue }) {
 
     const [provenanceldata, setProvenancedata] = useState([]);
 
@@ -15,7 +15,7 @@ function Provenance() {
     }, []);
     return (
 
-        <select className={style.Div_select}>
+        <select className={style.Div_select} onChange={(event) => provenanceValue(event.target.value)}>
             <option selected >Select</option>
             {provenanceldata && provenanceldata.map((data) =>
 
